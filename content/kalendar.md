5e080171eaf15cf722fa3912e87a62f683211216
Tlapička <marek@tlapicka.net>
2015-04-30 11:56:19 +0200

    první nástřel

---
+++ content/kalendar.md
@@ -0,0 +1,42 @@
+Title: Kalendář
+Date: 2015-04-30 09:39
+Category: úloha
+Tags: podmínky, cykly, mírně pokročilý
+Author: Marek Nožka
+
+
+Vypište na obrazovku kalendář ve tvaru:
+
+    Po Út St Čt Pá So Ne
+        1  2  3  4  5  6  
+     7  8  9 10 11 12 13  
+    14 15 16 17 18 19 20  
+    21 22 23 24 25 26 27  
+    28 29 30 31  
+  
+Tato úloha nespočívá v pouhém nalezení modulu (Calendar), který kalendář
+umí vypsat! Je potřeba jej opravdu naprogramovat.
+
+Aktuální datum, nebo měsíc zjistíte pomocí modulu `datetime`:
+<http://docs.python.org/2.7/library/datetime.html>, ale není to až tak zapotřebí. 
+Pro začátek stačí, když program bude načítat první den v týdnu, který připadá 
+na první den měsíce. Například program, generující výše zmíněnou ukázku by přijal 
+jako vstup číslo `2`, protože měsíc začíná úterkem.
+
+
+    >>> import datetime
+    >>> today=datetime.date.today()
+    >>> today.month
+    10
+    >>> today.day
+    30
+    >>> today.year
+    2012
+    >>> today.weekday()
+    1
+    >>> datetime.date(2012,10,31).weekday()
+    2
+    >>> datetime.date(2012,10,28).weekday()
+    6
+    >>> dayCount=[None,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Marek'
+SITENAME = u'\xdalohy pro v\xfduku programov\xe1n\xed'
+SITEURL = ''
+
+PATH = 'content'
+
+THEME = 'ResponsivePelican'
+TIMEZONE = 'Europe/Prague'
+
+DEFAULT_LANG = u'cs'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         )
+
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/spseol/'),
+          ('Facebook', 'https://cs-cz.facebook.com/spseol'),
+          ('G+', 'https://plus.google.com/118347382558771670752'),
+          )
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+MD_EXTENSIONS = [
+    'codehilite(css_class=highlight)',
+    'extra',
+    'headerid(level=2)',
+    'toc',
+    'linksShortcuts:Shortcuts',
+]
+
+STATIC_PATHS = ['extra', ]
+EXTRA_PATH_METADATA = {'extra/README': {'path': 'README.md'},
+                       'extra/.nojekyll': {'path': '.nojekyll'},
+                       }
